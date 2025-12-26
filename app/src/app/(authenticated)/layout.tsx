2a10ac14ea8a19930e728e12830a0ecf946f4d09
Stefan Ratchev <stefan@zonguru.com>
2025-12-26 17:04:54 +0200

    feat: wrap layout in MobileNavProvider, responsive margins

--- app/src/app/(authenticated)/layout.tsx
+++ app/src/app/(authenticated)/layout.tsx
@@ -1,5 +1,6 @@
 import { getCurrentUser } from "@/lib/user";
 import { Sidebar } from "@/components/layout/Sidebar";
+import { MobileNavProvider } from "@/contexts/MobileNavContext";
 
 export default async function AuthenticatedLayout({
   children,
@@ -9,19 +10,21 @@ export default async function AuthenticatedLayout({
   const user = await getCurrentUser();
 
   return (
-    <div className="flex min-h-screen">
-      <Sidebar
-        user={{
-          name: user.name,
-          position: user.position,
-          initials: user.initials,
-          image: user.image,
-        }}
-        className="animate-slide-in"
-      />
-      <main className="flex-1 ml-[240px]">
-        <div className="px-6 py-5">{children}</div>
-      </main>
-    </div>
+    <MobileNavProvider>
+      <div className="flex min-h-screen">
+        <Sidebar
+          user={{
+            name: user.name,
+            position: user.position,
+            initials: user.initials,
+            image: user.image,
+          }}
+          className="animate-slide-in"
+        />
+        <main className="flex-1 lg:ml-[240px]">
+          <div className="px-3 py-4 md:px-4 lg:px-6 lg:py-5">{children}</div>
+        </main>
+      </div>
+    </MobileNavProvider>
   );
 }
